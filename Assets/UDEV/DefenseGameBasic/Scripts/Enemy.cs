84ab450fd9a71c79bba6b676d22cf5eff4fc1d85
Duyy285 <106329709+duyanh285@users.noreply.github.com>
2022-07-14 20:48:25 +0700

    up
    up

---
+++ Assets/UDEV/DefenseGameBasic/Scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DA.DefrnseBasic
+{
+    public class Enemy : MonoBehaviour
+    {
+        public float speed;
+        public float atkDistance;//khoang cach enemy tan cong
+        private Animator m_amin;
+        private Rigidbody2D m_rb;
+        private Player m_player;
+
+        private void Awake()
+        {
+            m_amin = GetComponent<Animator>();
+            m_rb = GetComponent<Rigidbody2D>();
+            m_player = FindObjectOfType<Player>();
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (m_rb == null || m_player == null) return;
+
+            if (m_rb)
+                m_rb.velocity = new Vector2(-speed, m_rb.velocity.y);
+
+            if (Vector2.Distance(m_player.transform.position, 
+                transform.position) <= atkDistance)
+            {
+               // if (m_amin)
+                    m_amin.SetBool(Const.ATTACK_ANIM, true);
+
+                m_rb.velocity = Vector2.zero;//vecto toa do (0,0)
+            }
+            else
+            {
+                m_rb.velocity = new Vector2(-speed, m_rb.velocity.y);
+            }
+        }
+    }
+}
--- Assets/UDEV/DefenseGameBasic/Scripts/Player.cs
+++ Assets/UDEV/DefenseGameBasic/Scripts/Player.cs
@@ -2,49 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+namespace DA.DefrnseBasic
 {
-    public float atkRate;
-    private Animator m_anim;
-    private float m_curAtkRate;
-    private bool m_isAttacked;
+    public class Player : MonoBehaviour
+    {
+        public float atkRate;
+        private Animator m_anim;
+        private float m_curAtkRate;
+        private bool m_isAttacked;
 
-    private void Awake()
-    {
-        m_anim = GetComponent<Animator>();
-        m_curAtkRate = atkRate;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)&& !m_isAttacked)//trang thai tan cong
+        private void Awake()
         {
-            // Debug.Log("Nguoi choi da bam chuot trai");
-            if (m_anim)
-                m_anim.SetBool(Const.ATTACK_ANIM, true);
-            m_isAttacked = true;
+            m_anim = GetComponent<Animator>();
+            m_curAtkRate = atkRate;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+
         }
 
-        if (m_isAttacked)
+        // Update is called once per frame
+        void Update()
         {
-            m_curAtkRate -= Time.deltaTime;
-            if (m_curAtkRate <= 0)
+            if (Input.GetMouseButtonDown(0) && !m_isAttacked)//trang thai tan cong
             {
-                m_isAttacked = false;
-                m_curAtkRate = atkRate;
+                // Debug.Log("Nguoi choi da bam chuot trai");
+                if (m_anim)
+                    m_anim.SetBool(Const.ATTACK_ANIM, true);
+                m_isAttacked = true;
+            }
+
+            if (m_isAttacked)
+            {
+                m_curAtkRate -= Time.deltaTime;
+                if (m_curAtkRate <= 0)
+                {
+                    m_isAttacked = false;
+                    m_curAtkRate = atkRate;
+                }
             }
         }
-    }
 
-    public void ResetAtkAnim()//ve trang thai binh thuong
-    {
-        if (m_anim)
-            m_anim.SetBool(Const.ATTACK_ANIM, false);
+        public void ResetAtkAnim()//ve trang thai binh thuong
+        {
+            if (m_anim)
+                m_anim.SetBool(Const.ATTACK_ANIM, false);
+        }
     }
 }
