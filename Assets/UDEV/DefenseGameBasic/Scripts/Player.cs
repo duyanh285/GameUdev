b1230411b9947d2c562d799918d4ee43a68f8392
Duyy285 <106329709+duyanh285@users.noreply.github.com>
2022-07-14 20:27:09 +0700

    up

---
+++ Assets/UDEV/DefenseGameBasic/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float atkRate;
+    private Animator m_anim;
+    private float m_curAtkRate;
+    private bool m_isAttacked;
+
+    private void Awake()
+    {
+        m_anim = GetComponent<Animator>();
+        m_curAtkRate = atkRate;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)&& !m_isAttacked)//trang thai tan cong
+        {
+            // Debug.Log("Nguoi choi da bam chuot trai");
+            if (m_anim)
+                m_anim.SetBool(Const.ATTACK_ANIM, true);
+            m_isAttacked = true;
+        }
+
+        if (m_isAttacked)
+        {
+            m_curAtkRate -= Time.deltaTime;
+            if (m_curAtkRate <= 0)
+            {
+                m_isAttacked = false;
+                m_curAtkRate = atkRate;
+            }
+        }
+    }
+
+    public void ResetAtkAnim()//ve trang thai binh thuong
+    {
+        if (m_anim)
+            m_anim.SetBool(Const.ATTACK_ANIM, false);
+    }
+}
