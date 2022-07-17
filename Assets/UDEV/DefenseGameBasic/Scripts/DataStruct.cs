798a9dcb7a0bf4aeebe481e8b5e175fdc58aa035
Duyy285 <106329709+duyanh285@users.noreply.github.com>
2022-07-17 11:15:41 +0700

    ShopManager
    ShopManager

---
+++ Assets/UDEV/DefenseGameBasic/Scripts/DataStruct.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DA.DefrnseBasic
+{
+    [System.Serializable]
+    public  class ShopItem
+    {
+        public Player playerPrefab;
+        public int price;
+        public Sprite previewImg;
+    }
+
+}
---
+++ Assets/UDEV/DefenseGameBasic/Scripts/ShopManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DA.DefrnseBasic
+{
+    public class ShopManager : MonoBehaviour
+    {
+        public ShopItem[] items;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            if (items == null || items.Length <= 0) return;
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                var item = items[i];
+                string dataKey = Const.PLAYER_PREFIX_PREF + i;//neu i = 1 player_0,player_1,player_2
+
+                if (item != null)
+                {
+                    if (i == 0)
+                        Pref.SetBool(dataKey,true);
+                    else
+                    {
+                        if (!PlayerPrefs.HasKey(dataKey))
+                            Pref.SetBool(dataKey, false);
+                    }
+
+                }
+            }
+        }
+    }
+}
